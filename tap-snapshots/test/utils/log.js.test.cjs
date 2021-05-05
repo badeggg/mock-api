605294a0423b7b55f7f75341f9ac903652c491d5
zhaoxuxu <zhaoxuxujc@gmail.com>
2021-05-05 12:31:52 +0800

    wip: steps to be formal: test suit for log.js

---
+++ tap-snapshots/test/utils/log.js.test.cjs
@@ -0,0 +1,21 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/utils/log.js TAP > msgMain 1`] = `
+DEBUG some debug message
+
+`
+
+exports[`test/utils/log.js TAP > msgMain 2`] = `
+INFO  some info
+
+`
+
+exports[`test/utils/log.js TAP > msgMain 3`] = `
+ERROR some error
+
+`
---
+++ test/utils/log.js
@@ -0,0 +1,23 @@
+const tap = require('tap');
+const log = require('../../src/utils/log.js');
+
+let output = [];
+
+// start hijack console.log function
+const originalStdoutWrite = process.stdout.write;
+process.stdout.write = (msg) => {output.push(msg)};
+
+log.debug('some debug message');
+log.info('some info');
+log.error('some error');
+
+// resume console.log function
+process.stdout.write = originalStdoutWrite;
+
+const dateStringEndIndex = output[0].indexOf('DEBUG');
+output.forEach(msg => {
+    const dateString = msg.slice(0, dateStringEndIndex);
+    const msgMain = msg.slice(dateStringEndIndex);
+    tap.ok(Date.parse(dateString), 'should log date first');
+    tap.matchSnapshot(msgMain, 'msgMain');
+});
