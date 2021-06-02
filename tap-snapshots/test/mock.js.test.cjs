f73da9052104fa479514b2c486390b4a3479b960
zhaoxuxu <zhaoxuxujc@gmail.com>
2021-06-02 15:03:56 +0800

    clean test output of mock.js

---
+++ tap-snapshots/test/mock.js.test.cjs
@@ -0,0 +1,24 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/mock.js TAP basic general function > log infos 1`] = `
+Array [
+  "info: \\u001b[32mmock-api listening on: 3001\\u001b[39m",
+]
+`
+
+exports[`test/mock.js TAP basic general function > log warnings 1`] = `
+Array [
+  "warning: Invalid json file '/fake-services/fake-api-path/response'.",
+]
+`
+
+exports[`test/mock.js TAP try next plus one port when current port is not available > log infos 1`] = `
+Array [
+  "info: \\u001b[32mmock-api listening on: 3001\\u001b[39m",
+]
+`
--- test/mock.js
+++ test/mock.js
@@ -4,6 +4,7 @@ const pathUtil = require('path');
 const tap = require('tap');
 const axios = require('axios');
 const tcpPortUsed = require('tcp-port-used');
+const removePathPrefix = require('./testUtils/removePathPrefix.js');
 
 tap.test('basic general function', async tap => {
     const fakeServicesDir = tap.testdir({
@@ -13,8 +14,14 @@ tap.test('basic general function', async tap => {
             },
         },
     });
+    let infoMsgs = [];
+    let warningMsgs = [];
     const mock = tap.mock('../src/mock.js', {
         '../src/utils/getProjectRoot.js': () => fakeServicesDir,
+        '../src/utils/log.js': {
+            info: (msg) => infoMsgs.push('info: ' + removePathPrefix(msg, fakeServicesDir)),
+            warn: (msg) => warningMsgs.push('warning: ' + removePathPrefix(msg, fakeServicesDir)),
+        },
     });
 
     const mockServer = await mock(process);
@@ -41,11 +48,15 @@ tap.test('basic general function', async tap => {
     const response = await axios.request(options);
     tap.equal(response.data, 'Some response content.');
 
+    tap.matchSnapshot(infoMsgs, 'log infos');
+    tap.matchSnapshot(warningMsgs, 'log warnings');
+
     mockServer.close();
     tap.end();
 });
 
 tap.test('try next plus one port when current port is not available', async tap => {
+    let infoMsgs = [];
     const inUsePort = 3000;
     const isInUse = await tcpPortUsed.check(inUsePort, '127.0.0.1');
     let portHolderServer;
@@ -65,12 +76,17 @@ tap.test('try next plus one port when current port is not available', async tap 
         }
         availablePort++;
     }
-    const mock = require('../src/mock.js');
+    const mock = tap.mock('../src/mock.js', {
+        '../src/utils/log.js': {
+            info: (msg) => infoMsgs.push('info: ' + msg),
+        },
+    });
     const mockServer = await mock(process);
     tap.equal(mockServer.address().port, availablePort,
         'should try next one port');
     if (portHolderServer)
         portHolderServer.close();
     mockServer.close();
+    tap.matchSnapshot(infoMsgs, 'log infos');
     tap.end();
 });
