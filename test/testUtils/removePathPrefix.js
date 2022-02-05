cb1c187592961a7fa176a2f5533b0b74f632eeb1
zhaoxuxu <zhaoxuxujc@gmail.com>
2022-02-05 12:44:09 +0800

    removePathPrefix.js for windows

--- test/testUtils/removePathPrefix.js
+++ test/testUtils/removePathPrefix.js
@@ -1,4 +1,10 @@
 module.exports = (path, prefix) => {
-    path = path.replace(new RegExp(prefix, 'g'), '');
+    /**
+     * String.prototype.replaceAll compatible with nodejs v15+. We take v12+ in consideration.
+     * @zhaoxuxu @2022-2-5
+     */
+    while (path.includes(prefix)) {
+        path = path.replace(prefix, '');
+    }
     return path;
 };
