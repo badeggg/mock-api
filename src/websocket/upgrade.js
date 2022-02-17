4c6420f2b3f26ece3d91ccbcf7ff2c5a3dde146d
zhaoxuxu <zhaoxuxujc@gmail.com>
2022-02-17 13:49:47 +0800

    websocket wip

--- src/websocket/upgrade.js
+++ src/websocket/upgrade.js
@@ -1,3 +1,31 @@
+const url = require('url');
+const _ = require('lodash');
+const cd = require('../utils/cd');
+const log = require('../utils/log.js');
+
 module.exports = (req, socket, head, wss) => {
-    // todo to continue
+    const parsedUrl = url.parse(req.url);
+    req.path = parsedUrl.pathname;
+    req.query = {};
+    for (const [key, value] of new URLSearchParams(parsedUrl.query)) {
+        req.query[key] = value;
+    }
+
+    let cdResult;
+    try {
+        cdResult = cd(req.path);
+    } catch (err) {
+        log.error(err.message);
+        if (err.name === 'NO-FAKE-SERVIVES-FOLDER') {
+            return {
+                resBody: err.message,
+                statusCode: 404,
+            };
+        }
+        throw err;
+    }
+    // if (!cdResult)
+    //     return null;
+
+    req.params = _.cloneDeep(cdResult.params);
 };
