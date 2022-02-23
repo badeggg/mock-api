3c40b9a635788649c95e605435d5af1f7ed26bb2
zhaoxuxu <zhaoxuxujc@gmail.com>
2022-02-23 16:10:57 +0800

    test suit: mock.js http buffer / metabox / js script log

---
+++ tap-snapshots/test/mock4.js.test.cjs
@@ -0,0 +1,36 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/mock4.js TAP response js result 3, buffer | log | metaBox > js export buffer 1`] = `
+你好
+`
+
+exports[`test/mock4.js TAP response js result 3, buffer | log | metaBox > js export buffer with debug log 1`] = `
+Object {
+  "data": Array [
+    1,
+    2,
+    3,
+  ],
+  "type": "Buffer",
+}
+`
+
+exports[`test/mock4.js TAP response js result 3, buffer | log | metaBox > js export func which return buffer 1`] = `
+你好
+`
+
+exports[`test/mock4.js TAP response js result 3, buffer | log | metaBox > js export metaBox 1`] = `
+
+`
+
+exports[`test/mock4.js TAP response js result 3, buffer | log | metaBox > log stdout 1`] = `
+Array [
+  "stdouts: debug log\\n",
+  "stdouts: delay debug log\\n",
+]
+`
--- test/mock4.js
+++ test/mock4.js
@@ -1,127 +1,101 @@
-// const pathUtil = require('path');
-// const tap = require('tap');
-// const axios = require('axios');
-// const removePathPrefix = require('./testUtils/removePathPrefix.js');
-// const transWindowsPath = require('./testUtils/transWindowsPath.js');
-// const obscureErrorStack = require('./testUtils/obscureErrorStack.js');
-// const _  = require('lodash');
-// 
-// tap.test('response js result 3, buffer', async tap => {
-//     const fakeServicesDir = tap.testdir({
-//         'fake-services': {
-//             'fake-api-path': {
-//                 'exportNum.js': `
-//                     module.exports = 1;
-//                 `,
-//                 'exportStr.js': `
-//                     module.exports = 'str';
-//                 `,
-//                 'exportObj.js': `
-//                     module.exports = {a: 12};
-//                 `,
-//                 'exportArr.js': `
-//                     module.exports = [1, null, {a: 56}];
-//                 `,
-//                 'exportNull.js': `
-//                     module.exports = null;
-//                 `,
-//                 'exportSymbol.js': `
-//                     module.exports = Symbol();
-//                 `,
-//                 'exportObjHasFunc.js': `
-//                     module.exports = {fn: () => {}};
-//                 `,
-//                 map: `
-//                     GET ?name=badeggg  -r ./exportNum.js
-//                     GET ?name=badeggg1 -r ./exportStr.js
-//                     GET ?name=badeggg2 -r ./exportObj.js
-//                     GET ?name=badeggg3 -r ./exportArr.js
-//                     GET ?name=badeggg4 -r ./exportNull.js
-//                     GET ?name=badeggg5 -r ./exportSymbol.js
-//                     GET ?name=badeggg6 -r ./exportObjHasFunc.js
-//                 `,
-//             },
-//         },
-//     });
-//     let warningMsgs = [];
-//     let errorMsgs = [];
-//     const mock = tap.mock('../src/mock.js', {
-//         '../src/utils/getProjectRoot.js': () => fakeServicesDir,
-//         '../src/utils/log.js': {
-//             info: () => {},
-//             warn: (msg) => warningMsgs.push('warning: '
-//                 + transWindowsPath(
-//                     removePathPrefix(msg, fakeServicesDir)
-//                 )
-//             ),
-//             error: (msg) => errorMsgs.push(
-//                 'error: ' + transWindowsPath(
-//                     removePathPrefix(
-//                         obscureErrorStack(msg),
-//                         pathUtil.resolve(fakeServicesDir, '../../')
-//                     )
-//                 )
-//             ),
-//         },
-//     });
-// 
-//     const mockServer = await mock(3051);
-// 
-//     const mockingLocation = `http://localhost:${mockServer.address().port}`;
-// 
-//     const optionsTemplate = {
-//         url: mockingLocation + '/fake-api-path',
-//         params: {name: 'badeggg'},
-//         method: 'GET',
-//     };
-// 
-//     const responses = await Promise.all([
-//         (() => {
-//             const options = _.cloneDeep(optionsTemplate);
-//             options.params.name = 'badeggg';
-//             return axios.request(options);
-//         })(),
-//         (() => {
-//             const options = _.cloneDeep(optionsTemplate);
-//             options.params.name = 'badeggg1';
-//             return axios.request(options);
-//         })(),
-//         (() => {
-//             const options = _.cloneDeep(optionsTemplate);
-//             options.params.name = 'badeggg2';
-//             return axios.request(options);
-//         })(),
-//         (() => {
-//             const options = _.cloneDeep(optionsTemplate);
-//             options.params.name = 'badeggg3';
-//             return axios.request(options);
-//         })(),
-//         (() => {
-//             const options = _.cloneDeep(optionsTemplate);
-//             options.params.name = 'badeggg4';
-//             return axios.request(options);
-//         })(),
-//         (() => {
-//             const options = _.cloneDeep(optionsTemplate);
-//             options.params.name = 'badeggg5';
-//             return axios.request(options);
-//         })(),
-//         (() => {
-//             const options = _.cloneDeep(optionsTemplate);
-//             options.params.name = 'badeggg6';
-//             return axios.request(options);
-//         })(),
-//     ]);
-//     tap.matchSnapshot(responses[0].data, 'js export number');
-//     tap.matchSnapshot(responses[1].data, 'js export string');
-//     tap.matchSnapshot(responses[2].data, 'js export object');
-//     tap.matchSnapshot(responses[3].data, 'js export array');
-//     tap.matchSnapshot(responses[4].data, 'js export null');
-//     tap.matchSnapshot(responses[5].data, 'js export symbol');
-//     tap.matchSnapshot(responses[6].data, 'js export objHasFunc');
-// 
-//     tap.matchSnapshot(warningMsgs, 'log warnings');
-//     tap.matchSnapshot(errorMsgs, 'log errors');
-// 
-//     mockServer.close();
-// });
+const tap = require('tap');
+const axios = require('axios');
+const _  = require('lodash');
+
+tap.test('response js result 3, buffer | log | metaBox', async tap => {
+    const fakeServicesDir = tap.testdir({
+        'fake-services': {
+            'fake-api-path': {
+                'buffer.js': `
+                    module.exports = Buffer.from('你好');
+                `,
+                'bufferFunc.js': `
+                    module.exports = () => {
+                        return Buffer.from('你好');
+                    };
+                `,
+                'metaBox.js': `
+                    module.exports = {
+                        isMetaBox: true,
+                        responseShouldEscapeBufferRecover: true,
+                        response: Buffer.from([1,2,3]),
+                    };
+                `,
+                'bufferLog.js': `
+                    module.exports = () => {
+                        console.log('debug log');
+                        setTimeout(() => {
+                            console.log('delay debug log');
+                        }, 500);
+                        return Buffer.from([1,2,3]);
+                    };
+                `,
+                map: `
+                    GET ?name=badeggg  -r ./buffer.js
+                    GET ?name=badeggg1 -r ./bufferFunc.js
+                    GET ?name=badeggg2 -r ./metaBox.js
+                    GET ?name=badeggg3 -r ./bufferLog.js
+                `,
+            },
+        },
+    });
+    let stdouts = [];
+    const mock = tap.mock('../src/mock.js', {
+        '../src/utils/getProjectRoot.js': () => fakeServicesDir,
+        process: {
+            ...process,
+            stdout: {
+                write: (out) => {
+                    if (out && out.byteLength)
+                        stdouts.push('stdouts: ' + out);
+                },
+            },
+        },
+        '../src/utils/log.js': {
+            info: () => {},
+            warn: () => {},
+            error: () => {},
+        },
+    });
+
+    const mockServer = await mock(3052);
+
+    const mockingLocation = `http://localhost:${mockServer.address().port}`;
+
+    const optionsTemplate = {
+        url: mockingLocation + '/fake-api-path',
+        params: {name: 'badeggg'},
+        method: 'GET',
+    };
+
+    const responses = await Promise.all([
+        (() => {
+            const options = _.cloneDeep(optionsTemplate);
+            options.params.name = 'badeggg';
+            return axios.request(options);
+        })(),
+        (() => {
+            const options = _.cloneDeep(optionsTemplate);
+            options.params.name = 'badeggg1';
+            return axios.request(options);
+        })(),
+        (() => {
+            const options = _.cloneDeep(optionsTemplate);
+            options.params.name = 'badeggg2';
+            return axios.request(options);
+        })(),
+        (() => {
+            const options = _.cloneDeep(optionsTemplate);
+            options.params.name = 'badeggg3';
+            return axios.request(options);
+        })(),
+    ]);
+    tap.matchSnapshot(responses[0].data, 'js export buffer');
+    tap.matchSnapshot(responses[1].data, 'js export func which return buffer');
+    tap.matchSnapshot(responses[2].data, 'js export buffer with debug log');
+    tap.matchSnapshot(responses[3].data, 'js export metaBox');
+
+    tap.matchSnapshot(stdouts, 'log stdout');
+
+    mockServer.close();
+});
